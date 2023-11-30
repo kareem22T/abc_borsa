82cdbc9127d600237579a95c9c5e65f0b40d8640
kareem22T <78378547+kareem22T@users.noreply.github.com>
2023-11-30 05:33:40 +0200

    first commit

---
+++ assets/js/main_listeners.js
@@ -0,0 +1,48 @@
+$(function () {
+    $('.left .social .more').on('click', function () {
+        $('.left .social .links').fadeToggle('fast')
+        if (!$('.left .social .links').hasClass('flex')) 
+            $('.left .social .links').toggleClass('flex')
+        else
+            setTimeout(() => {
+                $('.left .social .links').toggleClass('flex')
+            }, 200);
+    })
+    $('.top .right .more').on('click', function () {
+        $('.top .right .links').fadeToggle('fast')
+        if (!$('.top .right .links').hasClass('flex')) 
+            $('.top .right .links').toggleClass('flex')
+        else
+            setTimeout(() => {
+                $('.top .right .links').toggleClass('flex')
+            }, 200);
+    })
+    $('.bottom .right .more').on('click', function () {
+        $('.bottom .right .links, .bottom  .hide-content').fadeToggle('')
+        if (!$('.top .right .links').hasClass('')) 
+            $('.top .right .links').toggleClass('flex')
+        else
+            setTimeout(() => {
+                $('.top .right .links').toggleClass('flex')
+            }, 200);
+    })
+    $('.bottom .hide-content').on('click', function () {
+        $('.bottom .right .links, .bottom  .hide-content').fadeToggle('')
+        if (!$('.top .right .links').hasClass('')) 
+            $('.top .right .links').toggleClass('flex')
+        else
+            setTimeout(() => {
+                $('.top .right .links').toggleClass('flex')
+            }, 200);
+    })
+    $('.settings .more').on('click', function () {
+        $('.settings .settings_links').fadeToggle('fast')
+        if (!$('.settings .settings_links').hasClass('flex')) 
+            $('.settings .settings_links').toggleClass('flex')
+        else
+            setTimeout(() => {
+                $('.settings .settings_links').toggleClass('flex')
+            }, 200);
+    })
+    
+})
